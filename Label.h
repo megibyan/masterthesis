6af01df57419cefeef6296df68fa56b949d84981
megibyan <megibyan@gmail.com>
2013-08-21 15:38:51 +0200

    Create Label.h

---
+++ Label.h
@@ -0,0 +1,29 @@
+/**
+*	@file Label.h
+*	@brief file contains the definaion of the class Label.
+*
+*	@author Mikayel Egibyan
+*/
+
+#ifndef LABEL_H
+#define LABEL_H
+
+#include <iostream>
+#include <string>
+#include <QString>
+
+using namespace std;
+
+class Label
+{
+public:
+    Label();
+    Label(string);
+    void setValue(string);
+    string getValue();
+
+private:
+    string label;
+};
+
+#endif // LABEL_H
