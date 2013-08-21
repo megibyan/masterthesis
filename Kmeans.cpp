7b7b736ebd122f24aa7bf6da33b20296a46dfbaf
megibyan <megibyan@gmail.com>
2013-08-21 15:41:05 +0200

    Create Kmeans.cpp

---
+++ Kmeans.cpp
@@ -0,0 +1,12 @@
+#include "Kmeans.h"
+
+Kmeans::Kmeans()
+{
+
+}
+
+QVector<QPointF> Kmeans::kmean(QPointF points, int numberofClusters)
+{
+    QVector<QPointF> *overallClusters = new QVector<QPointF>();
+
+}
