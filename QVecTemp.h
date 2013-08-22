fd248908f6c9cfc80fd299486f8d4d9d40a50e6f
megibyan <megibyan@gmail.com>
2013-08-22 02:02:56 +0200

    Update QVecTemp.h

--- QVecTemp.h
+++ QVecTemp.h
@@ -1,3 +1,10 @@
+/**
+*	@file QVecTemp.h
+*	@brief file contains the definaion of the class QVecTemp
+*	@author Mikayel Egibyan
+*/
+
+
 #ifndef QVECTEMP_H
 #define QVECTEMP_H
 
@@ -9,7 +16,40 @@ template <class T>
 class QVecTemp
 {
 public:
-    static QVector<QVector<T> > SplitVector(QVector<T>, int);
+    static QVector<QVector<T> > SplitVector(QVector<T> items, int clustNum)
+    {
+        QVector< QVector<T> > allGroups;
+        //split the list into equal groups
+        int startIndex = 0;
+        int groupLength = (int)qRound((float)items.count() / (float)clustNum);
+        while (startIndex < items.count())
+        {
+            QVector<T> group;
+            //QVector<T> QVector::mid(int pos, int length = -1);
+            for(int i= startIndex; i<groupLength; i++)
+            {
+                group.append(items.at(i));
+            }
+            startIndex += groupLength;
+
+            //adjust group-length for last group
+            if (startIndex + groupLength > items.count())
+            {
+                groupLength = items.count() - startIndex;
+            }
+
+            allGroups.push_back(group);
+        }
+
+        //merge last two groups, if more than required groups are formed
+        if (allGroups.count() > clustNum && allGroups.count() > 2)
+        {
+            int num = allGroups.count() -2;
+            allGroups.insert(num, allGroups.last());
+            allGroups.remove(allGroups.count() - 1);
+        }
+        return (allGroups);
+    }
 private:
 };
 
