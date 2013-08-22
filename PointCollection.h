26507d3814174fc865844aa60863940166eab1dd
megibyan <megibyan@gmail.com>
2013-08-22 02:02:34 +0200

    Update PointCollection.h

--- PointCollection.h
+++ PointCollection.h
@@ -1,3 +1,9 @@
+/**
+*	@file PointCollection.h
+*	@brief file contains the definaion of the class PointCollection
+*	@author Mikayel Egibyan
+*/
+
 #ifndef POINTCOLLECTION_H
 #define POINTCOLLECTION_H
 
@@ -10,16 +16,15 @@ class PointCollection: public QVector<QPointF>
 public:
     PointCollection();
     void addPoint(QPointF);
-    QPointF deletePoint(int);
     QPointF deletePoint(QPointF);
     QPointF getNearestPointToCentroid();
     QPointF PointT;
-    QPointF getCenroid();
-    float calculateEuclideanDist(QPointF, QPointF);
-    void setCentoird(QPointF);
+    QPointF getCentroid();
+    void setCentroid(QPointF);
     void updateCentroid();
+    float calculateEuclideanDist(QPointF, QPointF);
 private:
-    QPointF centorid;
+    QPointF centroid;
 };
 
 #endif // POINTCOLLECTION_H
