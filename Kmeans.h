a972533ce62305b4642bbdd750446e335177db96
megibyan <megibyan@gmail.com>
2013-08-22 02:02:07 +0200

    Update Kmeans.h

--- Kmeans.h
+++ Kmeans.h
@@ -1,16 +1,26 @@
+/**
+*	@file Kmeans.h
+*	@brief file contains the definaion of the class Kmeans
+*	@author Mikayel Egibyan
+*/
+
 #ifndef KMEANS_H
 #define KMEANS_H
 
 #include <QVector>
 #include <QPointF>
+#include "QVecTemp.h"
 #include "PointCollection.h"
 
 class Kmeans
 {
 public:
-    Kmeans();
-    static QVector<QPointF> kmean(QPointF, int);
+    QVector<PointCollection> kmean(PointCollection, int);
+    int FindNearestCluster(QVector<PointCollection>, QPointF);
+    QVecTemp<QPointF> temp;
+    PointCollection w;
 private:
+
 };
 
 #endif // KMEANS_H
