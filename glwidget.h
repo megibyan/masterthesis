5ee20b44f35d256f046c8c06061188b12e7a9b1b
megibyan <megibyan@gmail.com>
2013-09-04 03:56:13 +0200

    04.09

--- glwidget.h
+++ glwidget.h
@@ -1,7 +1,6 @@
 /**
 *	@file glwidget.h
-*	@brief file contains the definaion of the class glwidget.
-*
+*	@brief file contains the definaion of the class glwidget
 *	@author Mikayel Egibyan
 */
 
@@ -17,6 +16,9 @@
 #include "Coordinate.h"
 #include "Scene.h"
 #include <QDebug>
+#include "CoordinateSystemCoordinates.h"
+#include <QtMath>
+#include "kmean_algorithm.h"
 
 class GLWidget : public QGLWidget
 {
@@ -27,14 +29,42 @@ public:
     void paintGL();
     void resizeGL(int w, int h);
     void updateGL_mech();
+    float getNormalizedWidth(int);
+    float getNormalizedHeight(int);
+    float rndup(float n);
     Coordinate coordinateT;
+    kmean_algorithm kmean;
+    void setQ(bool);
+    bool getQ();
+    void setPushedX(float);
+    float getPushedX();
+    void setPushedY(float);
+    float getPushedY();
+    void setCurrentX(float);
+    float getCurrentX();
+    void setCurrentY(float);
+    float getCurrentY();
+    void setReleasedX(float);
+    float getReleasedX();
+    void setReleasedY(float);
+    float getReleasedY();
+    void setHashToRender(QHash<QString, QVector<float> >);
+    QHash<QString, QVector<float> > getHashToRender();
+
 private:
+    bool q;
+    CoordinateSystemCoordinates maxCoordinate;
+    CoordinateSystemCoordinates minCoordinate;
     QTimer timer;
-    float eye_x;
-    float eye_y;
-    float eye_z;
-    int width;
-    int height;
+    float pushedX;
+    float pushedY;
+    float currentX;
+    float currentY;
+    float releasedX;
+    float releasedY;
+    float selectedPointX;
+    float selectedPointY;
+    QHash<QString, QVector<float> > hashToRender;
 };
 
 #endif // GLWIDGET_H
