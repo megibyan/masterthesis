bf96d95e15a38cd58f5a66f5c83fbfce726cbb7b
megibyan <megibyan@gmail.com>
2013-09-08 19:33:08 +0200

    Update glwidget.cpp

--- glwidget.cpp
+++ glwidget.cpp
@@ -31,11 +31,13 @@ GLWidget::GLWidget(QWidget *parent) :
 void GLWidget::updateGL_mech()
 {
     updateGL();
-    qDebug() << QWidget::width();
-    qDebug() << getNormalizedWidth(200);
-    qDebug() << selectedPointX << selectedPointY;
+    //coordinateT.setNumberOfIterations(kmean.getNumberOfIterations());
 }
 
+/**
+* This method is used to normalize the width
+* @author Mikayel Egibyan
+*/
 float GLWidget::getNormalizedWidth(int value)
 {
     float a = (float)value;
@@ -44,6 +46,10 @@ float GLWidget::getNormalizedWidth(int value)
     return (a/(QWidget::width()))*(max-min) + min;
 }
 
+/**
+* This method is used to normalize the height
+* @author Mikayel Egibyan
+*/
 float GLWidget::getNormalizedHeight(int value)
 {
     float a = (float)value;
@@ -52,81 +58,140 @@ float GLWidget::getNormalizedHeight(int value)
     return (a/(QWidget::height()))*(max-min) + min;
 }
 
+/**
+* This method is used to round up a float number
+* @author Mikayel Egibyan
+*/
 float GLWidget::rndup(float n)
 {
     return floor(n*10000+0.5)/10000;
 }
 
+/**
+* This method is used set rendering togle
+* @author Mikayel Egibyan
+*/
 void GLWidget::setQ(bool value)
 {
     q = value;
 }
 
+/**
+* This method is used get rendering togle
+* @author Mikayel Egibyan
+*/
 bool GLWidget::getQ()
 {
     return q;
 }
 
+/**
+* This method is used set the mouse press coordinate
+* @author Mikayel Egibyan
+*/
 void GLWidget::setPushedX(float value)
 {
     pushedX = value;
 }
 
+/**
+* This method is used get the mouse press coordinate
+* @author Mikayel Egibyan
+*/
 float GLWidget::getPushedX()
 {
     return pushedX;
 }
 
+/**
+* This method is used set the mouse press coordinate
+* @author Mikayel Egibyan
+*/
 void GLWidget::setPushedY(float value)
 {
     pushedY = value;
 }
 
+/**
+* This method is used get the mouse press coordinate
+* @author Mikayel Egibyan
+*/
 float GLWidget::getPushedY()
 {
     return pushedY;
 }
 
+/**
+* This method is used set the mouse current coordinate
+* @author Mikayel Egibyan
+*/
 void GLWidget::setCurrentX(float value)
 {
     currentX = value;
 }
 
+/**
+* This method is used get the mouse current coordinate
+* @author Mikayel Egibyan
+*/
 float GLWidget::getCurrentX()
 {
     return currentX;
 }
 
+/**
+* This method is used set the mouse current coordinate
+* @author Mikayel Egibyan
+*/
 void GLWidget::setCurrentY(float value)
 {
     currentY = value;
 }
 
+/**
+* This method is used get the mouse current coordinate
+* @author Mikayel Egibyan
+*/
 float GLWidget::getCurrentY()
 {
     return currentY;
 }
 
+/**
+* This method is used set the mouse release coordinate
+* @author Mikayel Egibyan
+*/
 void GLWidget::setReleasedX(float value)
 {
     releasedX = value;
 }
 
+/**
+* This method is used get the mouse release coordinate
+* @author Mikayel Egibyan
+*/
 float GLWidget::getReleasedX()
 {
     return releasedX;
 }
 
+/**
+* This method is used set the mouse release coordinate
+* @author Mikayel Egibyan
+*/
 void GLWidget::setReleasedY(float value)
 {
      releasedY = value;
 }
 
+/**
+* This method is used gset the mouse release coordinate
+* @author Mikayel Egibyan
+*/
 float GLWidget::getReleasedY()
 {
     return releasedY;
 }
-
 
 /**
 * This method is used to initialize the GL
@@ -152,9 +217,11 @@ void GLWidget::paintGL()
     if(coordinateT.getRenderTogle())
     {
         coordinateT.render_data_points(QWidget::width(), QWidget::height(), getHashToRender());
+        coordinateT.render_centroids(QWidget::width(), QWidget::height());
     }
     coordinateT.render_rect(QWidget::width(), QWidget::height(), q, pushedX, pushedY, currentX, currentY);
     coordinateT.selectPoints(QWidget::width(), QWidget::height(), pushedX, pushedY, releasedX, releasedY);
+    coordinateT.selectCentroid(QWidget::width(), QWidget::height(), pushedX, pushedY);
     coordinateT.renderLabelForPoint(QWidget::width(), QWidget::height(), coordinateT.getSelectedPointX(), coordinateT.getSelectedPointY(), coordinateT.getSelectedPointXOriginal(), coordinateT.getSelectedPointYOriginal());
 }
 
@@ -172,12 +239,37 @@ void GLWidget::resizeGL(int w, int h)
     glLoadIdentity();
 }
 
+/**
+* This method is used set the number of iterations
+* @author Mikayel Egibyan
+*/
+void GLWidget::setNumberOfIteration(int value)
+{
+    numberOfiterations = value;
+}
 
+/**
+* This method is used get the number of iterations
+* @author Mikayel Egibyan
+*/
+int GLWidget::getNumberOfIteration()
+{
+    return numberOfiterations;
+}
+
+/**
+* This method is used set the data that must be rendered
+* @author Mikayel Egibyan
+*/
 void GLWidget::setHashToRender(QHash<QString, QVector<float> > hash)
 {
     hashToRender = hash;
 }
 
+/**
+* This method is used get the data that must be rendered
+* @author Mikayel Egibyan
+*/
 QHash<QString, QVector<float> > GLWidget::getHashToRender()
 {
     return hashToRender;
